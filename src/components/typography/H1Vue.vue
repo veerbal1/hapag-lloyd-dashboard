13a919aa57b288d39e36f9fa106feaa64c08794d
veerbal1 <veerbal1singh1@gmail.com>
2022-08-20 23:49:06 +0530

    Created dummy title component

---
+++ src/components/typography/H1Vue.vue
@@ -0,0 +1,18 @@
+<template>
+    <h1>{{ text }}</h1>
+</template>
+
+<script>
+export default {
+    name: 'H1Vue',
+    props: ["text"]
+}
+</script>
+
+<style>
+h1 {
+    text-transform: uppercase;
+    font-weight: 700;
+    font-size:2em;
+}
+</style>
