7c2ac3b54d6fa18a148155ee28695ba40cd639b2
veerbal1 <veerbal1singh1@gmail.com>
2022-08-20 23:48:47 +0530

    Created dummy textfield

---
+++ src/components/inputs/TextField.vue
@@ -0,0 +1,22 @@
+<template>
+    <input type="text">
+</template>
+
+<script>
+export default {
+    name: 'TextField',
+}
+</script>
+
+<style scoped>
+input {
+    width: 100%;
+    height: 100%;
+    border: none;
+    border-radius: 5px;
+    outline: none;
+    font-size: 1.5em;
+    font-weight: lighter;
+    padding: 0.2em;
+}
+</style>
