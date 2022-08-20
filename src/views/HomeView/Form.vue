d25f8c25494963c9709aee1f8d2d05565994c16a
veerbal1 <veerbal1singh1@gmail.com>
2022-08-21 00:53:43 +0530

    Added submit fn on submit button

--- src/views/HomeView/Form.vue
+++ src/views/HomeView/Form.vue
@@ -2,8 +2,8 @@
     <div id="form">
         <H1Vue text="Welcome" />
         <text-field placeholder="Email" v-model="email" type="email" messages="Email: admin@hapag.co" />
-        <text-field placeholder="Password" v-model="password" type="password" messages="Password: 12345678"/>
-        <va-button color="warning" class="mr-4 mb-2" :rounded="false">Log In</va-button>
+        <text-field placeholder="Password" v-model="password" type="password" messages="Password: 12345678" />
+        <va-button color="warning" class="mr-4 mb-2" :rounded="false" @click.prevent="submit">Log In</va-button>
         <va-button v-show="false" color="warning" class="mr-4 mb-2" :rounded="false" loading size="medium">Log In
         </va-button>
     </div>
@@ -19,6 +19,12 @@ export default {
             email: 'admin@hapag.co',
             password: '12345678',
         };
+    },
+    methods: {
+        submit() {
+            console.log('Submit');
+            this.$router.push('/dashboard');
+        },
     },
     components: {
         TextField,
