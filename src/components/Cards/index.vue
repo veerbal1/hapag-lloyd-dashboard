185f8869541e32b4f9eb3eec226f71759ff55928
veerbal1 <veerbal1singh1@gmail.com>
2022-08-22 22:38:01 +0530

    Created Balance and Active order card

---
+++ src/components/Cards/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="w-full max-w-xs shadow-md p-6">
+    <h1 class="text-xl antialiased font-normal">{{ title }}</h1>
+    <h1 class="text-4xl antialiased font-normal mb-2">
+      {{ value }}
+    </h1>
+    <h6 class="text-sm font-thin">{{ secondaryTitle }}</h6>
+    <h6 class="text-sm font-thin">&euro; {{ secondaryValue }}</h6>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BalanceCard',
+  props: ["title", "value", "secondaryTitle", "secondaryValue"],
+  data() {
+    return {
+      currentBalance: 312312,
+      averageBalance: 336699
+    }
+  }
+}
+</script>
+
+<style>
+</style>
