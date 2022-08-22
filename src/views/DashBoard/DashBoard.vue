c54cda1ad171da81b940a64dde0789c82ce3da5d
veerbal1 <veerbal1singh1@gmail.com>
2022-08-22 22:38:28 +0530

    Updated dashboard for cards

--- src/views/DashBoard/DashBoard.vue
+++ src/views/DashBoard/DashBoard.vue
@@ -1,10 +1,11 @@
 <template>
     <Hapag-Header />
     <div class="w-full p-4 flex" style="gap:1em;flex:1">
-        <section id="left">
-            <balance-card />
+        <section id="left" class="flex-col" style="display:flex;flex: 0 0 auto;">
+            <card title="Balance" value="312,312" secondaryTitle="Average Monthly" secondaryValue="336,699" />
+            <card title="Active Order" value="CNC Crank Grinding" secondaryTitle="Charges" secondaryValue="450" />
         </section>
-        <section id="right" style="gap:1em;flex:5">
+        <section id="right" style="gap:1em;flex:8">
             <section id="right-top">Right Top</section>
             <section id="right-bottom">Right Bottom</section>
         </section>
@@ -12,13 +13,13 @@
 </template>
 
 <script>
-import HapagHeader from '../../components/Header/HeaderFile.vue'
-import BalanceCard from '../../components/Cards/Balance.vue'
+import HapagHeader from '../../components/Header/HeaderFile.vue';
+import Card from '../../components/Cards/index.vue';
 export default {
     components: {
-        BalanceCard,
-        HapagHeader
-    }
+        Card,
+        HapagHeader,
+    },
 }
 </script>
 
