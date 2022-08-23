3afd691cc792fa525a9906d39ed1b2ad2e50299e
veerbal1 <veerbal1singh1@gmail.com>
2022-08-23 21:21:07 +0530

    Added donut chart to show last three month order categories

---
+++ src/components/Cards/CategoryChart.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="w-full max-w-xs shadow-md p-6">
+        <canvas id="category-chart" width="272" height="272">
+
+        </canvas>
+    </div>
+</template>
+
+<script>
+import { Chart, registerables } from 'chart.js';
+Chart.register(...registerables);
+export default {
+    name: 'CategoryChart',
+    mounted: () => {
+        const ctx = document.getElementById('category-chart').getContext('2d');
+        new Chart(ctx, {
+            type: 'doughnut',
+            data: {
+                labels: ['Pharmaceutical Products', 'Fruits', 'Paints', 'Juices', 'Dry Fruits'],
+                datasets: [{
+                    label: '# of Votes',
+                    data: [1, 19, 3, 5, 2],
+                    backgroundColor: ['#EE6082', '#F49E3F', '#F9CC53', '#52C0C0', '#339AF7'],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                responsive: true,
+                plugins: {
+                    legend: {
+                        position: 'top',
+                    },
+                    title: {
+                        display: true,
+                        text: 'Last 6 Months - No. of Containers'
+                    }
+                }
+            }
+        })
+        console.log('Chart mounted', ctx)
+    }
+}
+</script>
+
+<style>
+</style>
--- src/views/DashBoard/DashBoard.vue
+++ src/views/DashBoard/DashBoard.vue
@@ -4,6 +4,7 @@
         <section id="left" class="flex-col" style="display:flex;flex: 0 0 auto;">
             <card title="Balance" value="312,312" secondaryTitle="Average Monthly" secondaryValue="336,699" />
             <card title="Recent Order" value="Crankshaft CNC" secondaryTitle="Charges" secondaryValue="450" />
+            <category-chart />
         </section>
         <section id="right" style="gap:1em;flex:8">
             <section id="right-top">
@@ -17,12 +18,14 @@
 <script>
 import HapagHeader from '../../components/Header/HeaderFile.vue';
 import Card from '../../components/Cards/index.vue';
-import ShipStatus from '../../components/ShipStatus/index.vue'
+import ShipStatus from '../../components/ShipStatus/index.vue';
+import CategoryChart from '../../components/Cards/CategoryChart.vue'
 export default {
     components: {
         Card,
         HapagHeader,
-        ShipStatus
+        ShipStatus,
+        CategoryChart
     },
 }
 </script>
